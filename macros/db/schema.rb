5aa1c8130d227fc5f7764b626736b0c4fe172f0c
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2016-03-10 14:51:37 -0600

    Adds user and meal migrations

--- macros/db/schema.rb
+++ macros/db/schema.rb
@@ -11,6 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160310204037) do
+
+  create_table "meals", force: :cascade do |t|
+    t.string   "name",       limit: 25
+    t.string   "type",       limit: 255, default: "Snack", null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name", limit: 25
+    t.string   "last_name",  limit: 50
+    t.string   "email",      limit: 100
+    t.string   "password",   limit: 40
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
